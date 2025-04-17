6d022645870b11b31c606e4ac3f4e97d9679fc07
Cbermont <89049830+bermont13@users.noreply.github.com>
2025-04-17 23:20:24 +0000

    tabla trabajos terminada

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -10,10 +11,38 @@ class User(db.Model):
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
-
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Trabajo(db.Model):
+    __tablename__ = "trabajos"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    modalidad: Mapped[str] = mapped_column(String(120), nullable=False)
+    nombre_puesto: Mapped[str] = mapped_column(String(120), nullable=False)
+    remuneracion: Mapped[int] = mapped_column(nullable=True)
+    condiciones: Mapped[str] = mapped_column(String(250), nullable=False)
+    responsabilidades: Mapped[str] = mapped_column(String(250), nullable=False)
+    requerimientos: Mapped[str] = mapped_column(String(250), nullable=False)
+    activo: Mapped[bool] = mapped_column(Boolean(), default=True)
+    fecha_inicio: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    fecha_vencimiento: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "modalidad": self.modalidad,
+            "nombre_puesto": self.nombre_puesto,
+            "remuneracion": self.remuneracion,
+            "condiciones": self.condiciones,
+            "responsabilidades": self.responsabilidades,
+            "requerimientos": self.requerimientos,
+            "activo": self.activo,
+            "fecha_inicio": self.fecha_inicio.isoformat() if self.fecha_inicio else None,
+            "fecha_vencimiento": self.fecha_vencimiento.isoformat() if self.fecha_vencimiento else None
+        }
+    
