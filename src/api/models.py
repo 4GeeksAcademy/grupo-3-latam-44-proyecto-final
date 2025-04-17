e8338fa5ab2dcd461c0d8d3d7effade5e80d3687
Sebastian Cascante <139644093+devSebaKsk@users.noreply.github.com>
2025-04-17 20:18:42 +0000

    feat: db favoritos

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,14 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Favorites(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    id_trabajo: Mapped[int] = mapped_column(nullable=False)
+    id_trabajador: Mapped[int] = mapped_column(nullable=False)
 
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "id_trabajo": self.id_trabajor,
+            "id_trabajador": self.id_trabajador,
         }
