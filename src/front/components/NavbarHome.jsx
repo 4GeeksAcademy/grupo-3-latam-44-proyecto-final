ea6b1d016ba5aa2e010e72aa3d9187d0b436f382
Sebastian Cascante <139644093+devSebaKsk@users.noreply.github.com>
2025-04-22 04:11:59 +0000

    landing page v1

---
+++ src/front/components/NavbarHome.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+export const NavbarHome = () => {
+    return (
+        <nav className="navbar bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <img src="/src/front/assets/img/logo.png" alt="Logo" width="30" height="24" className="d-inline-block align-text-top" />
+                    JobFinder
+                </a>
+                <div className="d-flex">
+                    <Link to="/login-empresa"><button className="btn btn-outline-success me-2" type="button">Login Empresa</button></Link>
+                    <Link to="/login"><button className="btn btn-success me-2" type="button">Login</button></Link>
+                </div>
+            </div>
+
+        </nav>
+    )
+}
