54433c846558032d668ba4b006952230dc677535
Sebastian Cascante <139644093+devSebaKsk@users.noreply.github.com>
2025-04-28 00:32:08 +0000

    Fix: Unificar login empresa y login user

--- src/front/pages/RegistroUser.jsx
+++ src/front/pages/RegistroUser.jsx
@@ -1,9 +1,11 @@
 // src/front/assets/pages/RegistroUser.jsx
 import RegisterFormUser from "../components/RegisterFormUser";
+import { NavbarRegistro } from '../components/NavbarRegistro';
 
 function RegistroUser() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <NavbarRegistro/>
       <RegisterFormUser />
     </div>
   );
